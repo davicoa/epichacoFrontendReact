baeb93c78316e961868f896da89fcf07c6a5f25a
Bran <bbogdanich42@gmail.com>
2020-07-07 01:16:32 -0300

    redirect admin/ logout

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./style.css";
+import { useHistory } from "react-router-dom";
+import AuthService from "services/AuthService" 
 const user = JSON.parse(localStorage.getItem("user"));
 
 const Header = () => {
+  const history = useHistory();
+
+  const routeChange = () => {
+    let path = `admin`;
+    history.push(path);
+    window.location.reload();
+  };
+
   return (
     <header>
       <div className="headerContainer fullCenter">
@@ -12,10 +22,25 @@ const Header = () => {
       </div>
       {user && (
         <div className="logoutAdministrar">
-          {user.roles.some(role => role === "ROLE_ADMIN" || role === "ROLE_MODERATOR") && (
-            <span className="logoutAdministrar2"> Administrar </span>
+          {user.roles.some(
+            (role) => role === "ROLE_ADMIN" || role === "ROLE_MODERATOR"
+          ) && (
+            
+            <span
+              className="logoutAdministrar2"
+              onClick={() => routeChange("admin")}
+            >
+              Administrar
+            </span>
           )}
-          <span className="logoutAdministrar2">Logout</span>
+          <span
+           className="logoutAdministrar2"
+           onClick={() => {
+            AuthService.logout()
+            window.location.reload();
+           }}
+           >
+             Logout</span>
         </div>
       )}
     </header>
