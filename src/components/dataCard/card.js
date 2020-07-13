1506810c81081c06ed7a3489dd731195ec4203a0
davicoa <alvarado_z@outlook.com>
2020-07-13 15:28:28 -0300

    peso de imgs

--- src/components/dataCard/card.js
+++ src/components/dataCard/card.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 import ReactApexChart from "react-apexcharts";
 import ClipLoader from "react-spinners/ClipLoader";
-import logoChaco from "images/ISO_Mesadetrabajo_1.png";
+import logoChaco from "images/logoChaco.png";
 import logoEscudo from "images/logo_escudo.png";
 import OptionsGraph from "constant/optionsGraphs";
 import ApiGraphsDataService from "services/ApiGraphsDataService";
