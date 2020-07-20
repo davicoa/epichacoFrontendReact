7398dbbee194cf53aeebfe811388724ad1c4581e
davicoa <alvarado_z@outlook.com>
2020-07-20 09:35:27 -0300

    randon

--- src/pages/screen/admin/AdmintrarUsuarios/Vistas/usuario.js
+++ src/pages/screen/admin/AdmintrarUsuarios/Vistas/usuario.js
@@ -4,16 +4,18 @@ import "../style.css";
 
 const ForestalPrimario = (props) => {
   const obj = props.obj;
-  
+
   const [username, setUsername] = useState(typeof obj !== "undefined" ? obj.username : "");
   const [email, setEmail] = useState(typeof obj !== "undefined" ? obj.email : "");
   const [password, setPassword] = useState("");
   const [rpassword, setRPassword] = useState("");
   const [role, setRole] = useState(typeof obj !== "undefined" ? obj.role : "");
 
+  const [randon, setRandon] = useState("");
+
   const savetobd = (e) => {
     e.preventDefault();
-    if (password === rpassword) {   
+    if (password === rpassword) {
       props.saveToDb('auth/signup', {
         username,
         email,
@@ -50,6 +52,17 @@ const ForestalPrimario = (props) => {
     }
   };
 
+  function randomPass(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log("--->", result)
+    return setRandon(result);
+  }
+
   return (
     <div className="formContainer">
       <span className="tituloDatoACargar">Usuario</span>
@@ -57,7 +70,7 @@ const ForestalPrimario = (props) => {
         <div className="divContaniner">
           <p className="textinput">Nombre de Usuario</p>
           <input
-            placeholder="usuario con el que se ingresa"
+            placeholder="Nombre de Usuario"
             type="text"
             value={username}
             name="username"
@@ -73,6 +86,16 @@ const ForestalPrimario = (props) => {
             name="email"
             onChange={setImputHandler}
           />
+        </div>
+        <div className="divContaniner">
+          <p className="textinput"></p>
+          <input
+            placeholder="Randon"
+            type="text"
+            value={randon}
+            readOnly
+          />
+          <button type="reset" onClick={() => randomPass(8)}>Randon</button>
         </div>
         <div className="divContaniner">
           <p className="textinput">Contraseña</p>
